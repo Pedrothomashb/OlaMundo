56594607380354ca38ceb6913ce9977db7e2bc36
unknown <pthbeskow@minha.fag.edu.br>
2026-03-04 19:53:13 -0300

    Cria a classe Hello

---
+++ Hello.java
@@ -0,0 +1,14 @@
+public class Hello 
+{
+    public static void main(String[] args) 
+    {
+        HelloWorld();
+    }
+
+    public static void HelloWorld()
+    {
+        System.out.println("Olá, Mundo!");
+    }
+}
+
+
