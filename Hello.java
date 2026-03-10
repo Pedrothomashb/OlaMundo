95cd55216b3bcbd450dc52972d3ce9ab158d1ff3
unknown <pedro.beskow1706@gmail.com>
2026-03-10 19:44:39 -0300

    Adiciona Italiaano

--- Hello.java
+++ Hello.java
@@ -1,11 +1,16 @@
-public class Hello
-{
-    public static void main(String[] args) 
-    {
-        HelloWorld();
+public class Hello{
+    public static void main(String[] args) {
+        Italiano.helloWorld();
     }
 
     public static void HelloWorld(){
         System.out.println("Hello World!");
+    }
+
+    public class Italiano{
+
+        public static void helloWorld(){
+            System.out.println("Ciao Mondo!");
+        }
     }
 }
