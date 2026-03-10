190da61785c9341d36327f7bb12fd35f518d0a11
unknown <pedro.beskow1706@gmail.com>
2026-03-10 19:34:31 -0300

    Adiciona HelloWorld

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello
+{
+    public static void main(String[] args) 
+    {
+        HelloWorld();
+    }
+
+    public static void HelloWorld(){
+        System.out.println("Hello World!");
+    }
+}
